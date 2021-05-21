25d623d634e2944eddadc13da26d2887349f11ba
rcmohan12 <roopamohan12@gmail.com>
2021-05-21 21:56:12 +0530

    - bfs with adjacency matrix

--- src/main/java/com/rcm/dsa/graphs/UnDirectedGraph.java
+++ src/main/java/com/rcm/dsa/graphs/UnDirectedGraph.java
@@ -17,10 +17,28 @@ public class UnDirectedGraph {
         adjList.add(new Integer[]{6});
         adjList.add(new Integer[]{2});
 
+        int[][] adjMatrix = {{0,1,0,1,0,0,0,0,0},
+                {1,0,0,0,0,0,0,0,0},
+                {0,0,0,1,0,0,0,0,1},
+                {1,0,1,0,1,1,0,0,0},
+                {0,0,0,1,0,0,1,0,0},
+                {0,0,0,1,0,0,0,0,0},
+                {0,0,0,0,1,0,0,1,0},
+                {0,0,0,0,0,0,1,0,0},
+                {0,0,1,0,0,0,0,0,0}
+        };
+
         int[] elements = breadthFirstSearch(adjList);
-        System.out.println("BFS");
+        System.out.println("BFS Adjacency list");
         for(int x = 0; x < elements.length; x++) {
             System.out.print(" "+ elements[x]);
+        }
+
+        System.out.println("");
+        int[] els = breadthFirstSearchAdjacencyMatrix(adjMatrix);
+        System.out.println("BFS Adjacency Matrix");
+        for(int x = 0; x < els.length; x++) {
+            System.out.print(" "+ els[x]);
         }
 
         List<Integer> elements1 = depthFirstSearch(adjList,0, new ArrayList<Integer>(), new HashSet<Integer>());
@@ -66,6 +84,52 @@ public class UnDirectedGraph {
         }
         return res;
     }
+
+    /**
+     * Breadth first search starting from node 0
+     * * * * * 1 * * 4 --------- 6
+     * * * * / * * / * * * * * * |
+     * * * 0 --- 3 ----- 5 * * * 7
+     * * * * * * / * * * * * * * *
+     * * * * * 2 ----- 8 * * * * *
+     * BFS order --> 0, 1, 3, 4, 5, 2, 6, 8, 7
+     *
+     * @param adjMatrix : adjacency matrix for each element in the graph
+     * @return : elements ordered by BFS
+     */
+    public static int[] breadthFirstSearchAdjacencyMatrix(int[][] adjMatrix) {
+        int[] res = new int[adjMatrix.length];
+        Set<Integer> visited = new HashSet<Integer>();
+        Queue<Integer> queue = new LinkedList();
+
+        int[] row = adjMatrix[0];
+        queue.add(0);
+        visited.add(0);
+        int count = 0;
+        for(int x = 0; x < row.length; x++) {
+            if(row[x] == 1) {
+                queue.add(x);
+            }
+        }
+
+       while(!queue.isEmpty()) {
+           int el = queue.remove();
+           res[count] = el;
+           count++;
+           if(!visited.contains(el)) {
+               int[] r = adjMatrix[el];
+               for(int z = 0; z < r.length; z++) {
+                   if(r[z] == 1 && !visited.contains(z)) {
+                       queue.add(z);
+                   }
+               }
+               visited.add(el);
+           }
+       }
+
+        return res;
+    }
+
 
     /**
      * Breadth first search starting from node 0
